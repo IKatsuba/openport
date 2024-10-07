7ce537f53032c6760eee4d7f66e6bce456c0ccb3
Igor Katsuba <igor@katsuba.dev>
2024-10-07 21:45:52 +0300

    initial

---
+++ web/src/lib/client-manager.ts
@@ -0,0 +1,75 @@
+import { hri } from 'human-readable-ids';
+import Debug from 'debug';
+import Client from './client';
+import TunnelAgent from './tunnel-agent';
+
+interface ClientManagerOptions {
+  max_tcp_sockets?: number;
+}
+
+interface ClientInfo {
+  id: string;
+  port: number;
+  max_conn_count: number;
+  url?: string;
+}
+
+class ClientManager {
+  private opt: ClientManagerOptions;
+  private clients: Map<string, Client>;
+  stats: { tunnels: number };
+  private debug: Debug.Debugger;
+  private graceTimeout: NodeJS.Timeout | null;
+
+  constructor(opt: ClientManagerOptions = {}) {
+    this.opt = opt;
+    this.clients = new Map<string, Client>();
+    this.stats = { tunnels: 0 };
+    this.debug = Debug('lt:ClientManager');
+    this.graceTimeout = null;
+  }
+
+  async newClient(id: string): Promise<ClientInfo> {
+    if (this.clients.has(id)) {
+      id = hri.random();
+    }
+
+    const maxSockets = this.opt.max_tcp_sockets || 10;
+    const agent = new TunnelAgent({ clientId: id, maxTcpSockets: maxSockets });
+
+    const client = new Client({ id, agent });
+    this.clients.set(id, client);
+
+    client.once('close', () => {
+      this.removeClient(id);
+    });
+
+    try {
+      const info = await agent.listen();
+      this.stats.tunnels++;
+      return { id, port: info.port, max_conn_count: maxSockets };
+    } catch (err) {
+      this.removeClient(id);
+      throw err;
+    }
+  }
+
+  removeClient(id: string): void {
+    this.debug('removing client: %s', id);
+    const client = this.clients.get(id);
+    if (!client) return;
+    this.stats.tunnels--;
+    this.clients.delete(id);
+    client.close();
+  }
+
+  hasClient(id: string): boolean {
+    return this.clients.has(id);
+  }
+
+  getClient(id: string): Client | undefined {
+    return this.clients.get(id);
+  }
+}
+
+export default ClientManager;
---
+++ web/src/lib/client.ts
@@ -0,0 +1,149 @@
+import http from 'http';
+import net from 'net';
+import Debug from 'debug';
+import pump from 'pump';
+import EventEmitter from 'events';
+import TunnelAgent from './tunnel-agent';
+
+interface ClientOptions {
+  agent: TunnelAgent;
+  id: string;
+}
+
+// A client encapsulates req/res handling using an agent
+//
+// If an agent is destroyed, the request handling will error
+// The caller is responsible for handling a failed request
+class Client extends EventEmitter {
+  private readonly agent: TunnelAgent;
+  private readonly id: string;
+  private readonly debug: Debug.Debugger;
+  private graceTimeout: NodeJS.Timeout;
+
+  constructor(options: ClientOptions) {
+    super();
+
+    this.agent = options.agent;
+    this.id = options.id;
+
+    this.debug = Debug(`lt:Client[${this.id}]`);
+
+    // client is given a grace period in which they can connect before they are _removed_
+    this.graceTimeout = setTimeout(() => {
+      this.close();
+    }, 1000).unref();
+
+    this.agent.on('online', () => {
+      this.debug('client online %s', this.id);
+      clearTimeout(this.graceTimeout);
+    });
+
+    this.agent.on('offline', () => {
+      this.debug('client offline %s', this.id);
+
+      // if there was a previous timeout set, we don't want to double trigger
+      clearTimeout(this.graceTimeout);
+
+      // client is given a grace period in which they can re-connect before they are _removed_
+      this.graceTimeout = setTimeout(() => {
+        this.close();
+      }, 1000).unref();
+    });
+
+    // TODO(roman): an agent error removes the client, the user needs to re-connect?
+    // how does a user realize they need to re-connect vs some random client being assigned same port?
+    this.agent.once('error', () => {
+      this.close();
+    });
+  }
+
+  stats(): { connectedSockets: number } {
+    return this.agent.stats();
+  }
+
+  close(): void {
+    clearTimeout(this.graceTimeout);
+    this.agent.destroy();
+    this.emit('close');
+  }
+
+  handleRequest(req: http.IncomingMessage, res: http.ServerResponse): void {
+    this.debug('> %s', req.url);
+    const opt: http.RequestOptions = {
+      path: req.url,
+      agent: this.agent,
+      method: req.method,
+      headers: req.headers,
+    };
+
+    const clientReq = http.request(opt, (clientRes) => {
+      this.debug('< %s', req.url);
+      // write response code and headers
+      res.writeHead(clientRes.statusCode || 500, clientRes.headers);
+
+      // using pump is deliberate - see the pump docs for why
+      pump(clientRes, res);
+    });
+
+    // this can happen when underlying agent produces an error
+    // in our case we 504 gateway error this?
+    // if we have already sent headers?
+    clientReq.once('error', () => {
+      // TODO(roman): if headers not sent - respond with gateway unavailable
+    });
+
+    // using pump is deliberate - see the pump docs for why
+    pump(req, clientReq);
+  }
+
+  handleUpgrade(req: http.IncomingMessage, socket: net.Socket): void {
+    this.debug('> [up] %s', req.url);
+    socket.once('error', (err: NodeJS.ErrnoException) => {
+      // These client side errors can happen if the client dies while we are reading
+      // We don't need to surface these in our logs.
+      if (err.code === 'ECONNRESET' || err.code === 'ETIMEDOUT') {
+        return;
+      }
+      console.error(err);
+    });
+
+    this.agent.createConnection(
+      socket,
+      (err: Error | null, conn?: net.Socket) => {
+        this.debug('< [up] %s', req.url);
+        // any errors getting a connection mean we cannot service this request
+        if (err || !conn) {
+          socket.end();
+          return;
+        }
+
+        // socket might have disconnected while we were waiting for a connection
+        if (!socket.readable || !socket.writable) {
+          conn.destroy();
+          socket.end();
+          return;
+        }
+
+        // WebSocket requests are special in that we simply re-create the header info
+        // then directly pipe the socket data
+        // avoids having to rebuild the request and handle upgrades via the http client
+        const arr: string[] = [
+          `${req.method} ${req.url} HTTP/${req.httpVersion}`,
+        ];
+        for (let i = 0; i < req.rawHeaders.length - 1; i += 2) {
+          arr.push(`${req.rawHeaders[i]}: ${req.rawHeaders[i + 1]}`);
+        }
+
+        arr.push('');
+        arr.push('');
+
+        // using pump is deliberate - see the pump docs for why
+        pump(conn, socket);
+        pump(socket, conn);
+        conn.write(arr.join('\r\n'));
+      }
+    );
+  }
+}
+
+export default Client;
